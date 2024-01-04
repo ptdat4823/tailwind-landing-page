bcef63bd8878710fca3d98b6ffd6a600afdf38bf
ptdat4823 <phamtiendat4823@gmail.com>
2024-01-04 17:54:27 +0700

    he

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
@@ -19,8 +17,12 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Features />
+      <Pricing />
       <AboutSectionOne />
       <AboutSectionTwo />
+      <Testimonials />
+      <Video />
+      <Contact />
     </>
   );
 }
